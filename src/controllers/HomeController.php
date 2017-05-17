1d0711f7b08a40cdc66391a726b792b242806708
JFrederic <frederic-jouan97440@outlook.fr>
2017-05-17 16:02:59 +0400

    update

---
+++ src/controllers/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jouan
+ * Date: 26/04/17
+ * Time: 10:34
+ */
+
+namespace App\controllers;
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+
+class HomeController {
+
+
+    /**
+     * @param Request $request
+     * @param Application $app
+     * @return string
+     */
+    public function home(Request $request, Application $app)
+    {
+
+        return $app['twig']->render('homepage.html.twig');
+    }
+}
